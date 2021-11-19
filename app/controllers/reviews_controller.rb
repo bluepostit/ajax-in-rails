6f6b58778b70bc9fd62e0a4837dc86dd0511d57d
Yair <32864499+bluepostit@users.noreply.github.com>
2021-11-19 10:52:51 +0100

    update page with stimulus

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,10 +4,14 @@ class ReviewsController < ApplicationController
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review.restaurant = @restaurant
 
-    if @review.save
-      redirect_to restaurant_path(@restaurant, anchor: "review-#{@review.id}")
-    else
-      render 'restaurants/show'
+    respond_to do |format|
+      if @review.save
+        format.html { redirect_to restaurant_path(@restaurant, anchor: "review-#{@review.id}") }
+        format.json
+      else
+        format.html { render 'restaurants/show' }
+        format.json
+      end
     end
   end
 
